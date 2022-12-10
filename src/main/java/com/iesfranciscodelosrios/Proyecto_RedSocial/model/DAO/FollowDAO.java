e17be3c71f120042c09dbac2c3e581edaf476987
franciscoberral <fberzaf1908@g.educaand.es>
2022-12-10 16:18:26 +0100

    Modificado

--- src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/model/DAO/FollowDAO.java
+++ src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/model/DAO/FollowDAO.java
@@ -1,16 +1,19 @@
 package com.iesfranciscodelosrios.Proyecto_RedSocial.model.DAO;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import com.iesfranciscodelosrios.Proyecto_RedSocial.Conexion.Connection;
 import com.iesfranciscodelosrios.Proyecto_RedSocial.Interfaces.IFollowDAO;
 import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DataObject.Follow;
 import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DataObject.User;
 
-public class FollowDAO extends Follow implements IFollowDAO {
+public class FollowDAO extends Follow {
 
 	// CONSULTAS DE MariaDB
 	private final static String INSERT = "INSERT INTO Follow (id_user_follower, id_user_following) VALUES (?, ?)";
@@ -18,9 +21,15 @@ public class FollowDAO extends Follow implements IFollowDAO {
 	private final static String DELETE = "DELETE FROM Follow WHERE id_user_follower = ? AND id_user_following = ?";
 	private final static String FIND = "SELECT id, id_user_follower, id_user_following FROM Follow WHERE id = ?";
 	// FIN DE LAS CONSULTAS
+	
+	private Connection con;
+	public static EntityManager manager;
+	public static EntityManagerFactory emf;
 
 	// Constructores
-	public FollowDAO() {}
+	public FollowDAO() {
+		con = new Connection();
+	}
 
 	public FollowDAO(int id, User follower, User following) {
 		super(id, follower, following);
@@ -30,18 +39,23 @@ public class FollowDAO extends Follow implements IFollowDAO {
 		super(f.getId(), f.getFollower(), f.getFollowing());
 	}
 
+	/*
 	public FollowDAO(int id) {
 		this.find(id);
 	}
+	*/
 
 	/**
 	 * Este método nos sirve para insertar en la tabla Follow los seguidores y
 	 * seguidos de dicho usuario. 
 	 * @return true si se añade a los seguidores yseguidos, false si no se han insertado correctamente.
 	 */
-	@Override
 	public boolean create() {
-		return false;
+		if (con.insert(this)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	/**
@@ -49,9 +63,12 @@ public class FollowDAO extends Follow implements IFollowDAO {
 	 * seguidos. 
 	 * @return true si se elimina correctamente los seguidores y seguidos, false si no se ha eliminado.
 	 */
-	@Override
 	public boolean delete() {
-		return false;
+		if (con.delete(this)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	/**
@@ -59,9 +76,12 @@ public class FollowDAO extends Follow implements IFollowDAO {
 	 * por la id de la tabla Follow. 
 	 * @return true si se ha actualizado la tabla Follow, false si no se ha actualizado correctamente.
 	 */
-	@Override
 	public boolean update() {
-		return false;
+		if (con.update(this)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	/**
@@ -71,8 +91,8 @@ public class FollowDAO extends Follow implements IFollowDAO {
 	 * @param id el valor del campo por el que se obtiene. 
 	 * @return seguidores y seguidos obtenidos o null si no existen.
 	 */
-	@Override
-	public FollowDAO find(int id) {
-		return null;
+	public FollowDAO find() {
+		FollowDAO follow = (FollowDAO) con.find(this);
+		return follow;
 	}
 }

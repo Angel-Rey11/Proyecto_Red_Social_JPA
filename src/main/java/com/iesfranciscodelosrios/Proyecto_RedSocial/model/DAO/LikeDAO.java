0e73fce6f249605d2052242a2651aefb8c0e741d
franciscoberral <fberzaf1908@g.educaand.es>
2022-12-12 19:44:28 +0100

    Modificado

--- src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/model/DAO/LikeDAO.java
+++ src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/model/DAO/LikeDAO.java
@@ -1,21 +1,13 @@
 package com.iesfranciscodelosrios.Proyecto_RedSocial.model.DAO;
 
 import com.iesfranciscodelosrios.Proyecto_RedSocial.Conexion.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
-import com.iesfranciscodelosrios.Proyecto_RedSocial.Assets.DataService;
-import com.iesfranciscodelosrios.Proyecto_RedSocial.Interfaces.ILikeDAO;
 import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DataObject.Like;
 import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DataObject.Post;
-import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DataObject.User;
 
 /**
  * Clase LikeDAO que hereda de Like e implenta ILikeDAO
@@ -23,35 +15,40 @@ import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DataObject.User;
  *
  */
 public class LikeDAO {
-	private final static String GETALLLIKES = "SELECT * FROM Likes WHERE id_post = ";
-	private final static String FIND = "SELECT * FROM Likes WHERE id = ?";
-	private final static String COUNTLIKES = "SELECT COUNT(*) FROM likes WHERE id_post=?";
 	
-	private Connection con;
 	public static EntityManager manager;
-	public static EntityManagerFactory emf;
 	/**
 	 * Método para añadir un like
 	 * @return Si el like ha sido añadido
 	 */
-	public boolean create() {
-		if (con.insert(this)) {
-			return true;
-		} else {
-			return false;
-		}
+	public boolean create(Like l) {
+		boolean added = false;
+		
+		manager = Connection.getConnect().createEntityManager();
+		manager.getTransaction().begin();
+		manager.persist(l);
+		added = true;
+		manager.getTransaction().commit();
+		manager.close();
+		
+		return added;
 	}
 
 	/**
 	 * Método para borrar un like
 	 * @return Si el like ha sido borrado
 	 */
-	public boolean delete() {
-		if (con.delete(this)) {
-			return true;
-		} else {
-			return false;
-		}
+	public boolean delete(Like l) {
+		boolean removed = false;
+		
+		manager = Connection.getConnect().createEntityManager();
+		manager.getTransaction().begin();
+		manager.remove(l);
+		removed = true;
+		manager.getTransaction().commit();
+		manager.close();
+		
+		return removed;
 	}
 	
 	/**
@@ -59,11 +56,14 @@ public class LikeDAO {
 	 * @param id_post ID del post
 	 * @return Lista de likes del post
 	 */
-	public List<LikeDAO> getAllLikesbyPost() {
-		manager = emf.createEntityManager();
-		List<LikeDAO> likes = manager.createQuery(GETALLLIKES).getResultList();
+	public List<Like> getAllLikesbyPost(Post p) {
+		List<Like> list = new ArrayList<Like>();
+		
+		manager = Connection.getConnect().createEntityManager();
+		list = manager.createQuery("SELECT * FROM Likes WHERE id_post = " + p.getId()).getResultList();
 		manager.close();
-		return likes;
+		
+		return list;
 	}
 	
 	/**
@@ -71,15 +71,23 @@ public class LikeDAO {
 	 * @param id ID del like
 	 * @return Like encontrado
 	 */
-	public LikeDAO find() {
-		LikeDAO like = (LikeDAO) con.find(this.getId(),this.getClass());
-		return like;
+	public Like find(int id) {
+		Like l = null;
+		
+		manager = Connection.getConnect().createEntityManager();
+		l = manager.find(Like.class, id);
+		manager.close();
+		
+		return l;
 	}
 	
-	public int countLikes(int id_post) {
-		emf = Persistence.createEntityManagerFactory("sql");
-		manager = emf.createEntityManager();
-		int count = manager.createQuery(COUNTLIKES).getMaxResults();
+	public int countLikes(Post p) {
+		int count = 0;
+		
+		manager = Connection.getConnect().createEntityManager();
+		count = manager.createQuery("SELECT COUNT(*) FROM likes WHERE id_post = " + String.valueOf(p.getId())).getMaxResults();
+		manager.close();
+		
 		return count;
 	}
 }

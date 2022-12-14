ebd148585a51b0240ce11803e4e9c68502737964
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2022-12-14 14:39:25 +0100

    follows

--- src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/model/DAO/UserDAO.java
+++ src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/model/DAO/UserDAO.java
@@ -121,14 +121,47 @@ public class UserDAO extends DAO{
         manager.close();
         return misUsers;
     }
-    
-    public boolean addFollow(User u, User aux) {
+    /**
+     * 
+     * @param user_follower el usuario que va a ser follower
+     * @param user_followed el usuario al que vas a seguir
+     * @return true si todo ok
+     */
+    public boolean addFollow(User user_follower, User user_followed) {
     	boolean result = false;
+    	manager = Connection.getConnect().createEntityManager();
+    	//nuevo manager para crear modificaciones
+    	User followerManaged = manager.merge(user_follower);
+    	
+    	List<User> misFollows = followerManaged.getFollowing();
+    	manager.getTransaction().begin();
+    	user_follower.addFollowing(user_followed);
+        //manager.persist(u);
+        manager.getTransaction().commit();
+        manager.close();
+        result = true;
     	return result;
     }
     
+    /**
+     * Metodo para borrar follow
+     * @param u user logeado
+     * @param aux user que vamos a dejar de seguir
+     * @return true si todo lo ha hecho bien
+     */
     public boolean removeFollow(User u, User aux) {
     	boolean result = false;
+    	manager = Connection.getConnect().createEntityManager();
+    	u = manager.find(User.class, u.getId());
+    	if(manager.contains(u)) {
+    		List<User> misFollows = u.getFollowing();
+        	manager.getTransaction().begin();
+        	misFollows.remove(aux);
+        	u.setFollowing(misFollows);
+            manager.getTransaction().commit();
+            manager.close();
+            result = true;
+    	}
     	return result;
     }
 }
